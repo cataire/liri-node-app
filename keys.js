8bebb7931c65b6c38e17d912767015ee157aa6d6
cataire <mail.for.mu@gmail.com>
2018-01-29 17:58:08 -0700

    initial

---
+++ keys.js
@@ -0,0 +1,18 @@
+console.log('\nKeys are loaded');
+
+var twitterKeys = {
+  consumer_key: 'UZaYJWKeOTD0HFmgOwXzdE6p0',
+  consumer_secret: 'DxkGWN43MTTwcSchAAzFhrCZSkQwu4HOwxartsDcZE4rWQCYte',
+  access_token_key: '954037647705452544-dhyWLj9fKCVwpTd2v1tyZsbBZyqvbfl',
+  access_token_secret: 'onjIMpGhmAzyJchBc3eh7oVLjP2tHtLmS5blkd00WjDJv',
+}
+
+module.exports.twitterKeys = twitterKeys;
+
+var spotifyKeys = {
+	  id: "10963add41584d5bb55d619efa31f09d",
+  secret: "df3a373af5df419b914ef1b4e9c30e1d"
+}
+
+module.exports.spotifyKeys = spotifyKeys;
+
---
+++ liri.js
@@ -0,0 +1,201 @@
+const keys = require('./keys.js').twitterKeys;
+const spotifyKeys = require('./keys.js').spotifyKeys;
+const Twitter = require('twitter');
+const Spotify = require('node-spotify-api');
+const request = require('request');
+const fs = require('file-system');
+
+let command = process.argv[2];
+let search = process.argv.splice(3);
+
+function run(command, search) {
+
+		if (!command) {
+			console.log(`\nFor the last 20 tweets type "my-tweets"; 
+			\nTo find a song on Spotify type "spotify-this" and a song name;
+			\nTo find a movie type "movie-this" and a movie name;`);
+		}
+
+		else if (command === "my-tweets") {
+
+		searchTwitter();
+
+		}
+
+		else if (command === "spotify-this-song") {
+
+		searchSpotify(search);	
+
+		}
+
+		else if (command === "movie-this") {
+
+		searchOMDB(search);
+
+		}
+
+		else if (command === "do-what-it-says") {
+
+		doWhatItSays();
+			
+		}
+
+		else {
+			console.log(`\nLiri doesn't know that command.
+				\nFor the last 20 tweets type "my-tweets"; 
+			\nTo find a song on Spotify type "spotify-this" and a song name;
+			\nTo find a movie type "movie-this" and a movie name;`);
+		};
+
+};
+
+//=========== twitter ============//
+
+function searchTwitter() {
+
+	var client = new Twitter({
+	  consumer_key: keys.consumer_key,
+	  consumer_secret: keys.consumer_secret,
+	  access_token_key: keys.access_token_key,
+	  access_token_secret: keys.access_token_secret
+	});
+
+	client.get('search/tweets', {q: "BukashkaMu"}, function(error, tweets, response) {
+
+	if (error) {
+		console.log(error);
+	}
+
+	else {
+
+		for (var index = 0; index < tweets.statuses.length; index++) {
+			console.log("\n\n" + tweets.statuses[index].created_at);
+			console.log("\n" + tweets.statuses[index].text);
+
+		}
+		
+	};
+
+		fs.appendFile("log.txt", "\nTweets were searched", function(err) {
+			console.log("\nLog.txt updated")
+		});
+
+	});
+
+};
+
+
+
+//========= spotify =========================
+
+function searchSpotify(search) {
+
+	console.log(search);
+
+	if (!process.argv[3]) {
+		search = 'bill gates';
+	};
+
+	var spotify = new Spotify({
+	  id: spotifyKeys.id,
+	  secret: spotifyKeys.secret
+	});
+
+	spotify.search({ type: 'track', query: search }, function(error, data) {
+
+		if (error) {
+		    console.log(error);
+		}
+
+  		else {
+
+	  	console.log("\nSong name: " + data.tracks.items[0].name);
+		console.log("\nArtist: " + data.tracks.items[0].artists[0].name);
+		console.log("\nAlbum: " + data.tracks.items[0].album.name);
+		console.log("\nLink: " + data.tracks.items[0].href);
+
+		};
+
+		fs.appendFile("log.txt", "\n" + search, function(err) {
+			console.log("Log.txt updated");
+		});
+
+	});
+
+};
+
+
+
+// ============= OMDB ===========
+
+
+function searchOMDB(search) {	
+
+console.log(search);
+
+	// if (search === undefined) {
+	// 	search = 'Mr Nobody';
+	// };
+
+request('http://www.omdbapi.com/?apikey=trilogy&t=' + search, function (error, response, body) {
+
+
+	
+
+	if (error) {
+		console.log(error);
+	}
+
+	else {
+
+		let movieData = JSON.parse(body);
+		console.log("\n\nTitle: " + movieData.Title);
+		console.log("\nYear: " + movieData.Year);
+		console.log("\nIMDB Rating: " + movieData.imdbRating);
+		console.log("\nCountry: " + movieData.Country);
+		console.log("\nLanguage: " + movieData.Language);
+		console.log("\nPlot: " + movieData.Plot);
+		console.log("\nActors: " + movieData.Actors);
+		console.log("\nRotten Tomatoes Rating: " + movieData.Ratings[1].Value);
+
+	};
+
+		fs.appendFile("log.txt", "\n" + search, function(err) {
+			console.log("\nLog.txt updated");
+		});
+
+	});
+}
+
+
+
+function doWhatItSays() {
+
+fs.readFile("random.txt", 'utf8', function(error, data) {
+
+		if (!"random.txt") {
+			fs.writeFile("random.txt", "File created", function(error){
+				if (error) {
+					console.log(error);
+				}
+			});
+		}
+
+		if (error) {
+			console.log(error);
+		}
+		
+		else {
+			console.log(data);
+			let dataSplit = data.split(",");
+			search = dataSplit[1];
+			run(dataSplit[0], search);
+
+		};
+
+	});
+
+};
+
+
+run(command, search);
